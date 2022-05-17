be1c743975063b569c41c497d5f8bf167e3db9bd
kento-libra <teramoto1005@ruri.waseda.jp>
2022-05-17 15:45:48 +0900

    'ann'

--- elo.py
+++ elo.py
@@ -7,6 +7,7 @@ def main():
     manga=elo_calc.elo_calc('player.csv','result.csv')
     manga.debug()
     manga.fit()
-    manga.save_rating('elo_dummy.csv')
+    #manga.save_rating('elo_dummy.csv')
+    #manga.save_move('elo_dummy_mov.png')
 if __name__ == '__main__':
     main()
--- elo_calc.py
+++ elo_calc.py
@@ -1,16 +1,20 @@
 import pandas as pd
 import numpy as np
+import japanize_matplotlib
+import matplotlib.pyplot as plt
 
 def win_rate(Ra,Rb):
     return 1/(10**((Rb-Ra)/400)+1)
 
 class elo_calc:
     def __init__(self,player_dir,result_dir):
-        self.C=8
+        self.C=32.0
         self.player = pd.read_csv(player_dir)
         self.player = self.player.set_index('name')
         self.result = pd.read_csv(result_dir)
         self.player['elo']=1500.0
+        self.history = self.player['elo']
+        self.N = len(self.result.index)
 
     def debug(self):
         print(self.player)
@@ -25,9 +29,17 @@ class elo_calc:
             #print(delta_rate)
             self.player.at[winner,'elo']+=delta_rate
             self.player.at[loser,'elo']-=delta_rate
+            self.history = pd.concat([self.history,self.player['elo']],axis=1)
+            if 2*i/self.N > 1/self.C:
+                self.const_step()
         print(self.player)
-        
+        print(self.history)
+
     def const_step(self):
         self.C/=2
+        #print(self.C)
     def save_rating(self,save_dir):
         self.player.to_csv(save_dir)
+    def save_move(self,save_dir):
+        self.history.T.reset_index(drop=True).plot()
+        plt.savefig(save_dir)
