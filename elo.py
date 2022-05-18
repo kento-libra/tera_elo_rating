a5b664df8cfa8160d14f0041435240a282def296
kento-libra <teramoto1005@ruri.waseda.jp>
2022-05-18 19:08:22 +0900

    'fixing'

--- elo.py
+++ elo.py
@@ -7,9 +7,9 @@ import datetime
 def main():
     manga=elo_calc.elo_calc('player.csv','result.csv')
     #manga.debug()
-    start_time=datetime.datetime.now()
+    #start_time=datetime.datetime.now()
     manga.fit()
-    print(datetime.datetime.now()-start_time)
+    #print(datetime.datetime.now()-start_time)
     #manga.save_rating('elo_dummy.csv')
     #manga.save_move('elo_dummy_mov.png')
 if __name__ == '__main__':
