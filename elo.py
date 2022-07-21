e26e450a85872a8966963be45bc0c83bec8b7b12
kento-libra <teramoto1005@ruri.waseda.jp>
2022-07-21 13:27:33 +0900

    'integrate'

--- elo.py
+++ elo.py
@@ -6,6 +6,9 @@ import HandleRawCSV
 from constants import kEnqueteCsvPathPaperEnqueteData, kEnqueteCsvPathDigitalEnqueteData, kEnqueteCsvPathTitleData
 
 def main():
-    mycsv=HandleRawCSV.HandleRawCSV(kEnqueteCsvPathPaperEnqueteData, kEnqueteCsvPathDigitalEnqueteData, kEnqueteCsvPathTitleData)
+    #mycsv=HandleRawCSV.HandleRawCSV(kEnqueteCsvPathPaperEnqueteData, kEnqueteCsvPathDigitalEnqueteData, kEnqueteCsvPathTitleData)
+    myelo=elo_calc.elo_calc('player.csv','result.csv')
+    myelo.fit()
+    print(myelo.player)
 if __name__ == '__main__':
     main()
