f3974c20da7513c8b64771aa7ae3811c013ba316
kento-libra <teramoto1005@ruri.waseda.jp>
2022-07-22 18:14:48 +0900

    'integrated'

--- HandleRawCSV.py
+++ HandleRawCSV.py
@@ -9,9 +9,7 @@ import funcs
 
 class HandleRawCSV:
     def __init__(self,
-                kEnqueteCsvPathPaperEnqueteData,
-                kEnqueteCsvPathDigitalEnqueteData, 
-                kEnqueteCsvPathTitleData,
+                save_dir,
                 kEnqueteTitleMinimumIssue = 10,
                 kEnqueteTargetYear = 2021,
                 isWeightedByReadSegment=False,
@@ -21,76 +19,21 @@ class HandleRawCSV:
         self.isWeightedByReadSegment=isWeightedByReadSegment
         self.NumRandomLosers=NumRandomLosers
         self.division=division
-        enquete_data = pd.read_csv(kEnqueteCsvPathPaperEnqueteData)
-        enquete_data_digital = pd.read_csv(kEnqueteCsvPathDigitalEnqueteData)
-        title_data = pd.read_csv(kEnqueteCsvPathTitleData)
-        title_data_only = title_data[['title_code', 'title']]
-
-        enquete_data_merged = pd.merge(enquete_data, title_data_only, left_on='title_1', right_on='title_code', suffixes=['', '_1'])\
-                                .drop(columns=['title_code', 'title_1'])\
-                                .rename(columns={'title': 'title_1'})
-        enquete_data_merged = pd.merge(enquete_data_merged, title_data_only, left_on='title_2', right_on='title_code', suffixes=['', '_2'])\
-                                .drop(columns=['title_code', 'title_2'])\
-                                .rename(columns={'title': 'title_2'})
-        enquete_data_merged = pd.merge(enquete_data_merged, title_data_only, left_on='title_3', right_on='title_code', suffixes=['', '_3'])\
-                                .drop(columns=['title_code', 'title_3'])\
-                                .rename(columns={'title': 'title_3'})
-        # 読み切りなどを削除 'year == @kEnqueteTargetYear'
-        if division is not 'All':
-            enquete_data_digital=enquete_data_digital.query(division)
-            enquete_data_merged=enquete_data_merged.query(division)
-        self.enquete_data_digital_filtered = enquete_data_digital.query('not age.str.contains("\|")')\
-                                                    .groupby('title_1')\
-                                                    .filter(lambda x: x['issue'].groupby(x['title_1']).nunique().max() >= kEnqueteTitleMinimumIssue)
-        self.enquete_data_digital_filtered.loc[:,'read_segment'] = self.enquete_data_digital_filtered.loc[:,'read_segment'].replace('全部読んでいる', '25')
-        self.enquete_data_digital_filtered.loc[:,'read_segment'] = self.enquete_data_digital_filtered.loc[:,'age'].replace('(\d+).*', r'\1',regex=True)
-        self.enquete_data_digital_filtered.loc[:,'age'] = self.enquete_data_digital_filtered.loc[:,'age'].replace('(\d+).*', r'\1',regex=True)
-        self.enquete_data_filtered = enquete_data_merged\
-                                        .groupby('title_1')\
-                                        .filter(lambda x: x['issue'].groupby(x['title_1']).nunique().max() >= kEnqueteTitleMinimumIssue)
-        players_1 = self.enquete_data_filtered.groupby('title_1').groups.keys()
-        players_2 = self.enquete_data_filtered.groupby('title_2').groups.keys()
-        players_3 = self.enquete_data_filtered.groupby('title_3').groups.keys()
-        players_d1 = self.enquete_data_digital_filtered.groupby('title_1').groups.keys()
-        players_d2 = self.enquete_data_digital_filtered.groupby('title_2').groups.keys()
-        players_d3 = self.enquete_data_digital_filtered.groupby('title_3').groups.keys()
-
-        all_players = players_1 | players_2 | players_3 | players_d1 | players_d2 | players_d3
-
-        players = pd.DataFrame(all_players, columns=['name'])
-        players = players.set_index('name')
-        self.enquete_data_digital_filtered['issue']=(self.enquete_data_digital_filtered['year'].astype(str)+self.enquete_data_digital_filtered['issue'].astype(str).str.zfill(2)).astype(int)
-        self.enquete_data_filtered['issue']=(self.enquete_data_filtered['year'].astype(str)+self.enquete_data_filtered['issue'].astype(str).str.zfill(2)).astype(int)
-        self.enquete_data_filtered['read_segment']=1
-        #issue_list=enquete_data_digital_filtered.loc[:,'issue'].unique()
-        # unique_list=[]
-        # for i in issue_list:
-        #     issue_clip=enquete_data_digital_filtered.query('issue == {}'.format(i)).loc[:,'title_1':'title_3']
-        #     tmp=issue_clip['title_1']
-        #     tmp=pd.concat([tmp,issue_clip['title_2']])
-        #     tmp=pd.concat([tmp,issue_clip['title_3']])
-        #     issue_unique_list=tmp.unique()
-        #     unique_list.append([issue_unique_list,i])
-        # unique_list=pd.DataFrame(unique_list, columns=['list','issue'])
-        issue_num_digital = self.enquete_data_digital_filtered['issue']
-        issue_num_paper = self.enquete_data_filtered['issue']
-        self.issue_num_list = np.intersect1d(issue_num_digital.unique(), issue_num_paper.unique())
-        self.issue_num_list.sort()
+        #self.head_common='isWeight:{}_numLoser:{}_div:{}_'.format(self.isWeightedByReadSegment, self.NumRandomLosers,self.division)
+        self.enquete_data_digital_filtered, self.enquete_data_filtered, self.issue_num_list, self.head_common=\
+            funcs.read_raw_csv(save_dir, isWeightedByReadSegment=self.isWeightedByReadSegment, NumRandomLosers=self.NumRandomLosers,\
+                division=self.division, kEnqueteTitleMinimumIssue = kEnqueteTitleMinimumIssue)
         print('Initing Done!')
     
     def fit(self):
         results_digital=funcs.TranslateResult(self.enquete_data_digital_filtered,self.isWeightedByReadSegment,self.NumRandomLosers)
-        #results_paper=funcs.TranslateResult(self.enquete_data_filtered,self.isWeightedByReadSegment,self.NumRandomLosers)
+        results_paper=funcs.TranslateResult(self.enquete_data_filtered,self.isWeightedByReadSegment,self.NumRandomLosers)
         print('Translating Done!')
-        print(results_digital)
         self.elo_rating_digital = funcs.CalcElo(results_digital, self.issue_num_list)
-        #self.elo_rating_paper = funcs.CalcElo(results_paper, self.issue_num_list)
+        self.elo_rating_paper = funcs.CalcElo(results_paper, self.issue_num_list)
         print('Fitting Done!')
     def savepickle(self,pickle_dir):
-
-        head_common='isWeight:{}_numLoser:{}_div:{}_'.format(self.isWeightedByReadSegment, self.NumRandomLosers,self.division)
-
-        self.results_paper.to_pickle(pickle_dir + head_common +'results_paper.pickle')
-        self.results_digital.to_pickle(pickle_dir + head_common +'_results_digital.pickle')
-        self.elo_rating_paper.to_pickle(pickle_dir + head_common + '_elo_rating_paper_weight.pickle')
-        self.elo_rating_digital.to_pickle(pickle_dir + head_common +'_elo_rating_digital_weight.pickle')
+        self.results_paper.to_pickle(pickle_dir + self.head_common +'results_paper.pickle')
+        self.results_digital.to_pickle(pickle_dir + self.head_common +'_results_digital.pickle')
+        self.elo_rating_paper.to_pickle(pickle_dir + self.head_common + '_elo_rating_paper_weight.pickle')
+        self.elo_rating_digital.to_pickle(pickle_dir + self.head_common +'_elo_rating_digital_weight.pickle')
--- funcs.py
+++ funcs.py
@@ -1,3 +1,5 @@
+from genericpath import isfile
+import os
 import pandas as pd
 import numpy as np
 import japanize_matplotlib
@@ -5,6 +7,7 @@ import pickle
 from tqdm import tqdm
 import matplotlib.pyplot as plt
 import elo_calc
+from constants import kEnqueteCsvPathPaperEnqueteData, kEnqueteCsvPathDigitalEnqueteData, kEnqueteCsvPathTitleData, kEnqueteEloPickleBase
 
 
 def TranslateResult(enquete_data,isWeightedByReadSegment,NumRandomLosers):
@@ -42,7 +45,7 @@ def TranslateResult(enquete_data,isWeightedByReadSegment,NumRandomLosers):
 
 def CalcElo(results, issue_num_list):
     elo_rating = pd.DataFrame(columns = ['issue', 'name', 'rank', 'elo'])
-    for issue in tqdm(set(issue_num_list)):
+    for issue in tqdm(issue_num_list):
         results_by_issue = results.query('issue == @issue')[['win', 'lose','weight']]
         results_by_issue.reset_index(drop=True, inplace=True)
         # アンケートに登場するタイトルを抽出
@@ -63,5 +66,67 @@ def CalcElo(results, issue_num_list):
         elo_rating = elo_rating.append(res, ignore_index=True)
     return elo_rating
 
-def read_csv():
-    return
+filtered_digital_name='digital_filtered_raw.pickle'
+filtered_paper_name='paper_filtered_raw.pickle'
+issue_num_name='issue_num_list.npy'
+def read_raw_csv(save_dir,
+                isWeightedByReadSegment=False,
+                NumRandomLosers=0,
+                division='All',
+                kEnqueteTitleMinimumIssue = 10):
+    head_common ='isWeight:{}_numLoser:{}_div:{}_'.format(isWeightedByReadSegment, NumRandomLosers, division)
+    if os.path.isfile(save_dir + head_common + filtered_digital_name) and os.path.isfile(save_dir + head_common + filtered_paper_name) and os.path.isfile(save_dir + head_common + issue_num_name):
+        enquete_data_digital_filtered = pd.read_pickle(save_dir + head_common + filtered_digital_name)
+        enquete_data_filtered = pd.read_pickle(save_dir + head_common + filtered_paper_name)
+        issue_num_list = np.load(save_dir + head_common + issue_num_name)
+    else:
+        enquete_data = pd.read_csv(kEnqueteCsvPathPaperEnqueteData)
+        enquete_data_digital = pd.read_csv(kEnqueteCsvPathDigitalEnqueteData)
+        title_data = pd.read_csv(kEnqueteCsvPathTitleData)
+        title_data_only = title_data[['title_code', 'title']]
+
+        enquete_data_merged = pd.merge(enquete_data, title_data_only, left_on='title_1', right_on='title_code', suffixes=['', '_1'])\
+                                .drop(columns=['title_code', 'title_1'])\
+                                .rename(columns={'title': 'title_1'})
+        enquete_data_merged = pd.merge(enquete_data_merged, title_data_only, left_on='title_2', right_on='title_code', suffixes=['', '_2'])\
+                                .drop(columns=['title_code', 'title_2'])\
+                                .rename(columns={'title': 'title_2'})
+        enquete_data_merged = pd.merge(enquete_data_merged, title_data_only, left_on='title_3', right_on='title_code', suffixes=['', '_3'])\
+                                .drop(columns=['title_code', 'title_3'])\
+                                .rename(columns={'title': 'title_3'})
+        # 読み切りなどを削除 'year == @kEnqueteTargetYear'
+        if division is not 'All':
+            enquete_data_digital=enquete_data_digital.query(division)
+            enquete_data_merged=enquete_data_merged.query(division)
+        enquete_data_digital_filtered = enquete_data_digital.query('not age.str.contains("\|")')\
+                                                    .groupby('title_1')\
+                                                    .filter(lambda x: x['issue'].groupby(x['title_1']).nunique().max() >= kEnqueteTitleMinimumIssue)
+        enquete_data_digital_filtered.loc[:,'read_segment'] = enquete_data_digital_filtered.loc[:,'read_segment'].replace('全部読んでいる', '25')
+        enquete_data_digital_filtered.loc[:,'read_segment'] = enquete_data_digital_filtered.loc[:,'age'].replace('(\d+).*', r'\1',regex=True)
+        enquete_data_digital_filtered.loc[:,'age'] = enquete_data_digital_filtered.loc[:,'age'].replace('(\d+).*', r'\1',regex=True)
+        enquete_data_filtered = enquete_data_merged\
+                                        .groupby('title_1')\
+                                        .filter(lambda x: x['issue'].groupby(x['title_1']).nunique().max() >= kEnqueteTitleMinimumIssue)
+        players_1 = enquete_data_filtered.groupby('title_1').groups.keys()
+        players_2 = enquete_data_filtered.groupby('title_2').groups.keys()
+        players_3 = enquete_data_filtered.groupby('title_3').groups.keys()
+        players_d1 = enquete_data_digital_filtered.groupby('title_1').groups.keys()
+        players_d2 = enquete_data_digital_filtered.groupby('title_2').groups.keys()
+        players_d3 = enquete_data_digital_filtered.groupby('title_3').groups.keys()
+
+        all_players = players_1 | players_2 | players_3 | players_d1 | players_d2 | players_d3
+
+        players = pd.DataFrame(all_players, columns=['name'])
+        players = players.set_index('name')
+        enquete_data_digital_filtered['issue']=(enquete_data_digital_filtered['year'].astype(str)+enquete_data_digital_filtered['issue'].astype(str).str.zfill(2)).astype(int)
+        enquete_data_filtered['issue']=(enquete_data_filtered['year'].astype(str)+enquete_data_filtered['issue'].astype(str).str.zfill(2)).astype(int)
+        enquete_data_filtered['read_segment']=1
+        issue_num_digital = enquete_data_digital_filtered['issue']
+        issue_num_paper = enquete_data_filtered['issue']
+        issue_num_list = np.intersect1d(issue_num_digital.unique(), issue_num_paper.unique())
+        issue_num_list.sort()
+
+        enquete_data_digital_filtered.to_pickle(save_dir + head_common + filtered_digital_name)
+        enquete_data_filtered.to_pickle(save_dir + head_common + filtered_paper_name)
+        np.save(save_dir + head_common + issue_num_name ,issue_num_list)
+    return enquete_data_digital_filtered, enquete_data_filtered, issue_num_list, head_common
