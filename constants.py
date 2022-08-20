3d9c82fa5d524a75eae925e0516f2f8e48bc6d65
kento-libra <teramoto1005@ruri.waseda.jp>
2022-08-20 16:21:44 +0900

    CommitByBat

--- constants.py
+++ constants.py
@@ -5,7 +5,7 @@ from os import path
 
 # アンケートファイルの所在
 def dir_old():
-    kEnqueteDirectoryBase             = '/home/data/enquete'
+    kEnqueteDirectoryBase             = '/home/data/enquete/work/tera'
     kEnqueteDirectoryRaw              = path.join(kEnqueteDirectoryBase, 'raw')
     ComicSalses        = path.join(kEnqueteDirectoryRaw, 'comic_sales.csv')
     DigitalEnqueteData = path.join(kEnqueteDirectoryRaw, 'digital_enquete_data.csv')
@@ -16,10 +16,10 @@ def dir_old():
     return {'sales': ComicSalses,'digital':DigitalEnqueteData,'paper':PaperEnqueteData,\
         'title':TitleData,'pickle':PickleBase, 'img':ImgBase}
 def dir_new():
-    kEnqueteDirectoryBase             = '/home/data/enquete'
+    kEnqueteDirectoryBase             = '/home/data/enquete/work/tera'
     kEnqueteDirectoryRaw              = path.join(kEnqueteDirectoryBase, 'raw2')
     ComicSalses        = path.join(kEnqueteDirectoryRaw, 'タイトル巻数別売上資料.xlsx')
-    DigitalEnqueteData = path.join(kEnqueteDirectoryRaw, 'デジタルアンケート2022年18号以降.csv')
+    DigitalEnqueteData = path.join(kEnqueteDirectoryRaw, 'デジタルアンケート.csv')
     PaperEnqueteData   = path.join(kEnqueteDirectoryRaw, '紙アンケート5000件.csv')
     TitleData          = '/home/data/enquete/raw/title_data.csv'
     PickleBase ='/home/data/enquete/work/tera/pickle2/'
